7351fdbcd9c06580f036ca9dcb601b0d15329874
Alex Carrero <d200097@unimilitar.edu.co>
2022-11-21 22:50:12 -0600

    funciones agregar, editar, eliminar

--- peliculas/views.py
+++ peliculas/views.py
@@ -13,4 +13,22 @@ def peliculas(request):
     peliculas = Pelicula.objects.all()
     return render(request, 'peliculas/index.html', {'peliculas':peliculas})
 
+def agregar (request):
+    formulario = PeliculaForm(request.POST or None, request.FILES or None)
+    if formulario.is_valid():
+        formulario.save()
+        return redirect('peliculas')
+    return render(request, 'peliculas/agregar.html', {'formulario': formulario})
 
+def editar(request, id):
+    pelicula = Pelicula.objects.get(id = id)
+    formulario = PeliculaForm(request.POST or None, request.FILES or None, instance=pelicula)
+    if formulario.is_valid() and request.POST:
+        formulario.save()
+        return redirect('peliculas')
+    return render(request, 'peliculas/editar.html', {'formulario':formulario})
+
+def eliminar(request, id):
+    pelicula = Pelicula.objects.get(id = id)
+    pelicula.delete()
+    return redirect('peliculas')
