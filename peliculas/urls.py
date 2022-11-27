efc9a09639d5acda622b0306449745d4327b2e23
jacj14 <71483434+jacj14@users.noreply.github.com>
2022-11-27 12:16:59 -0600

    Paginas html creadas

--- peliculas/urls.py
+++ peliculas/urls.py
@@ -1,5 +1,7 @@
 from . import views
 from django.urls import path
+from django.conf import settings
+from django.contrib.staticfiles.urls import static 
 
 urlpatterns =[
     path('', views.inicio, name='inicio'),
@@ -8,4 +10,5 @@ urlpatterns =[
     path('peliculas/agregar', views.agregar, name='agregar'),
     path('peliculas/editar/<int:id>', views.editar, name='editar'),
     path('peliculas/eliminar/<int:id>', views.eliminar, name='eliminar')
-]
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
